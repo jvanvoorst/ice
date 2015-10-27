bbaff3cb74c52b181f64ab48766788fe9b32981b
Justin <justin.vanvoorst@gmail.com>
2015-10-27 17:18:09 -0600

    in the middle or passport

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-// Requires \\
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+
+// Connect to DB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ice')
+
+var passportConfig = require('./config/passport'); // Load in our passport configuration that decides how passport actually runs and authenticates
 
 // Create Express App Object \\
 var app = express();
+
+// Session Setup
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Application Configuration \\
 app.use(bodyParser.json());
@@ -11,13 +28,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 // Routes \\
+var authenticationCntrl = require('./controllers/authentication');
+
+// Our get request for viewing the login page
+// app.get('/auth/login', authenticationCntrl.login);
+
+// Post received from submitting the login form
+app.post('/auth/login', authenticationCntrl.processLogin);
+
+// Post received from submitting the signup form
+app.post('/auth/register', authenticationCntrl.processRegister);
+
+// Any requests to log out can be handled at this url
+app.get('/auth/logout', authenticationCntrl.logout);
+
+app.get('/loggedIn', authenticationCntrl.loggedIn)
+
+// This route is designed to send back the logged in user (or undefined if they are NOT logged in)
+app.get('/api/profile', authenticationCntrl.authorized, function(req, res){
+	res.send(req.user)
+})
+
 app.get('/', function(req, res){
   res.sendFile('/html/index.html', {root : './public'})
 });
+
+// ***** IMPORTANT ***** //
+// By including this middleware (defined in our config/passport.js module.exports),
+// We can prevent unauthorized access to any route handler defined after this call
+// to .use()
+app.use(passportConfig.ensureAuthenticated);
 
 // Creating Server and Listening for Connections \\
 var port = 3000
 app.listen(port, function(){
   console.log('Server running on port ' + port);
 
-})
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,45 @@
 var app = angular.module('iceApp', ['ngRoute']);
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', '$q', '$timeout', '$http', '$location', '$rootScope', function($routeProvider, $q, $timeout, $location, $rootScope) {
 
 	$routeProvider.when('/', {
 		templateUrl : '/html/welcome.html',
 		controller  : 'mainController'
 	})
-	$routeProvider.when('/register.html', {
+	.when('/register.html', {
 		templateUrl : '/html/register.html',
 		controller  : 'mainController'
 	})
-	$routeProvider.when('/login.html', {
+	.when('/login.html', {
 		templateUrl : '/html/login.html',
 		controller  : 'mainController'
 	})
+	.when('/profile.html', {
+		templateUrl : '/html/profile.html',
+		controller 	: 'profileController',
+		resolve     : { loggedIn : checkLoggedin }
+	})
+
+	var checkLoggedin = function($q, $timeout, $http, $location, $rootScope) {
+		console.log('check login')
+	 	// Initialize a new promise 
+	 	var deferred = $q.defer(); 
+	 	// Make an AJAX call to check if the user is logged in 
+	 	$http.get('/loggedIn').success(function(user) { 
+	 		// Authenticated 
+	 		if (user !== '0') deferred.resolve(); 
+	 			// Not Authenticated 
+	 		else { 
+	 			$rootScope.message = 'You need to log in.'; 
+	 			deferred.reject(); $location.url('/login'); 
+	 		} 
+	 	}); 
+	 	return deferred.promise; 
+	}; 
 
 }]);
 
-app.controller('mainController', ['$scope', '$http', function($scope, $http) {
+app.controller('mainController', ['$scope', '$http', '$location', function($scope, $http, $location) {
 
 	$scope.createUser = function() {
 		if ($scope.newUser.password !== $scope.newUser.passwordCheck) {
@@ -26,9 +48,23 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http) {
 			$scope.newUser.passwordCheck = '';
 		}
 		else {
-
+			$http.post('/auth/register', $scope.newUser).then(function(response) {
+				if (response.data.authorized) {
+					$location.url('/profile.html');
+				}
+			});
 		}
 	};
+
+}]);
+
+app.controller('profileController', ['$scope', '$http', '$location', '$q', '$timeout', '$rootScope', function($scope, $http, $location, $q, $timeout, $rootScope) {
+
+	$http.get('/api/profile').then(function(response) {
+		$scope.user = response.data;
+	})
+
+	
 
 }]);
 
