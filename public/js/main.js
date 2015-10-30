ad329bb4a97f6f1f5c8f29d11665c0c5b61f1014
Justin <justin.vanvoorst@gmail.com>
2015-10-30 12:18:54 -0600

    front end logic done

--- public/js/main.js
+++ public/js/main.js
@@ -22,6 +22,14 @@ app.config(['$routeProvider', function($routeProvider) {
 		templateUrl : '/html/editprofile.html',
 		controller  : 'editController'
 	})
+	.when('/receivers.html', {
+		templateUrl : '/html/receivers.html',
+		controller : 'profileController'
+	})
+	.when('/alerts.html', {
+		templateUrl : '/html/alerts.html',
+		controller : 'profileController'
+	})
 
 }]);
 
@@ -185,4 +193,47 @@ app.controller('editController', ['$scope', '$http', '$location', function($scop
 	};
 
 }]);
+
+// angular.module('plunker', ['ui.bootstrap', 'ui.bootstrap.datetimepicker'])
+
+// var DateTimePickerDemoCtrl = function ($scope, $timeout) {
+//   $scope.dateTimeNow = function() {
+//     $scope.date = new Date();
+//   };
+//   $scope.dateTimeNow();
+  
+//   $scope.toggleMinDate = function() {
+//     $scope.minDate = $scope.minDate ? null : new Date();
+//   };
+   
+//   $scope.maxDate = new Date('2014-06-22');
+//   $scope.toggleMinDate();
+
+//   $scope.dateOptions = {
+//     startingDay: 1,
+//     showWeeks: false
+//   };
+  
+//   $scope.$watch('date', function () {
+//      alert('changed');
+//   });
+  
+//   // Disable weekend selection
+//   $scope.disabled = function(calendarDate, mode) {
+//     return mode === 'day' && ( calendarDate.getDay() === 0 || calendarDate.getDay() === 6 );
+//   };
+  
+//   $scope.hourStep = 1;
+//   $scope.minuteStep = 15;
+
+//   $scope.timeOptions = {
+//     hourStep: [1, 2, 3],
+//     minuteStep: [1, 5, 10, 15, 25, 30]
+//   };
+
+//   $scope.showMeridian = true;
+//   $scope.timeToggleMode = function() {
+//     $scope.showMeridian = !$scope.showMeridian;
+//   };
+// };
 
